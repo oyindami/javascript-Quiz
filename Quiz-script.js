b0b2c2bffe4b1dd748d4547f7e64ab33cb4bf24d
Oyindamola Peterson <oyindamolapeterson@Oyindamolas-MacBook-Air.local>
2022-01-30 08:39:06 -0500

    uploaded worked on files to the repo

---
+++ Quiz-script.js
@@ -0,0 +1,224 @@
+//set containers for the values of quiz, results and the submit//
+//function for the quiz questions//
+//functionn for the results//
+
+// var quizcontainer = document.getElementById ('quiz');
+// var answercontainer = document.getElementById ('answer');
+// var submitcontainer = document.getElementById ('submit');
+
+// //timer
+// let timeleft = 75;
+// const downloadTimer = setInterval(function () {
+//   if (timeleft <= 0) {
+//     clearInterval(downloadTimer);
+//   }
+//   document.getElementById("time").innerHTML = `Time left : ${timeleft}`;
+//   //reduce 1 from the timer every one second
+//   timeleft -= 1;
+// }, 1000);
+
+// all the questions
+
+var QuizQuestions = [
+  {
+    question: "Which of these is not a code language?",
+    answers: ["1. Sequence", "2.  JavaScript", '3.  "HTML', '4.  "CSS'],
+    correctAnswer: "1. Sequence",
+  },
+  {
+    question: "Which of these is not CSS style?",
+    answers: ["1. boxstyle", "2. color", "3. eventlistener", "4. wrap"],
+    correctAnswer: "3. eventlistener",
+  },
+
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+      "1. Java Script",
+      "2. Terminal/Bash",
+      "3. For Loops",
+      "4. Console.log",
+    ],
+    correctAnswer: "4. Console.log",
+  },
+
+  {
+    question: "The conditions in an if/else statement is enclosed within:",
+    answers: [
+      "1. quotes",
+      "2. Curly Brackets",
+      "3. Parentheses",
+      "4. Square Brackets",
+    ],
+    correctAnswer: "3. Parentheses",
+  },
+];
+
+// function displayQuiz()
+// function displayResult()
+//to display the result//
+
+// submitButton.addEventListener('click', displayResult)
+
+//DEFINING THE questions
+
+//CREATING VARIABLES
+var timerRead = document.getElementById("time"); //variable for reading the time
+
+var containerDIVEL = document.getElementById("containerID"); //main body div
+
+var startBL = document.getElementById("startID"); //start div block
+var startBTNEL = document.getElementById("startBtn"); //start button
+var quizBL = document.getElementById("qandaDISID"); //quiz div
+var questionP1EL = document.getElementById("questionID");
+var answerULEL = document.getElementById("answersID");
+var a1BtnEL = document.getElementById("a1Btn");
+var a2BtnEL = document.getElementById("a2Btn");
+var a3BtnEL = document.getElementById("a3Btn");
+var a4BtnEL = document.getElementById("a4Btn");
+var ansValP1EL = document.getElementById("answerVID");
+var finalBL = document.getElementById("finalID");
+var finalScoreP1EL = document.getElementById("finalScore");
+var enterIntDivEL = document.getElementById("enterInt");
+var initialINPEL = document.getElementById("initialID");
+var submitIntBtnEL = document.getElementById("sumbitInt");
+
+var questionNum = 0; //putting the questions in globals
+var totalTime = 75; //setting the total amount of time allowed
+var timeRemaining = 0; // score catcher
+
+//create a time function to count the time up to 75 seconds
+function timeCounter() {
+  var timer = setInterval(function () {
+    if (totalTime >= 5) {
+      timerRead.textContent = "Time: " + totalTime;
+      totalTime--;
+      timeRemaining = totalTime;
+    } else {
+      console.log("Times Up!");
+      final();
+      timerRead.textContent = "Time: " + totalTime;
+    }
+  }, 1000);
+  console.log(timer);
+}
+//FINAL MESSAGE
+function final() {
+  containerDIVEL.removeChild(quizBL); //removing quiz
+  containerDIVEL.appendChild(finalBL); //adding final message
+  // Label
+  var createLabel = document.createElement("label");
+  createLabel.setAttribute("id", "createLabel");
+  createLabel.textContent = "Enter your initials: ";
+
+  finalBL.appendChild(createLabel);
+
+  // input
+  var createInput = document.createElement("input");
+  createInput.setAttribute("type", "text");
+  createInput.setAttribute("id", "initials");
+  createInput.textContent = "";
+
+  finalBL.appendChild(createInput);
+
+  // submit
+  var createSubmit = document.createElement("button");
+  createSubmit.setAttribute("type", "submit");
+  createSubmit.setAttribute("id", "Submit");
+  createSubmit.textContent = "Submit";
+
+  finalBL.appendChild(createSubmit);
+  // Event listener to capture initials and local storage for initials and score
+  createSubmit.addEventListener("click", function () {
+    var initials = createInput.value;
+    if (initials === null) {
+      console.log("No value entered!");
+    } else {
+      var finalScore = {
+        initials: initials,
+        score: timeRemaining,
+      };
+      console.log(finalScore);
+      var allScores = localStorage.getItem("allScores");
+      if (allScores === null) {
+        allScores = [];
+      } else {
+        allScores = JSON.parse(allScores);
+      }
+      allScores.push(finalScore);
+      var newScore = JSON.stringify(allScores);
+      localStorage.setItem("allScores", newScore); //setting items for local storage
+      // Travels to final page
+      window.location.replace("scores.html"); //calling on seperate location to call highscore
+    }
+  });
+}
+//PROCESSES CORRECT ANSWER VALIDATION
+function answerChecker(x) {
+  if (
+    QuizQuestions[questionNum].answers[x] === QuizQuestions[questionNum].correct
+  ) {
+    ansValP1EL.innerText = "Correct!";
+  } //if statement close
+  else {
+    ansValP1EL.innerText = "Wrong!";
+    totalTime = totalTime - 10;
+  } //else statement close
+}
+//PROCESSES QUESTION AND ANSWER FIELDS
+function quiz() {
+  //changing the questions and answers
+  if (questionNum <= 4) {
+    console.log(questionNum);
+    questionP1EL.innerHTML = QuizQuestions[questionNum].question;
+    console.log(QuizQuestions[questionNum].question);
+
+    a1BtnEL.innerText = QuizQuestions[questionNum].answers[0];
+    a2BtnEL.innerText = QuizQuestions[questionNum].answers[1];
+    a3BtnEL.innerText = QuizQuestions[questionNum].answers[2];
+    a4BtnEL.innerText = QuizQuestions[questionNum].answers[3];
+
+    a1BtnEL.addEventListener("click", function () {
+      answerChecker(0);
+      questionNum++;
+      quiz();
+    });
+    a2BtnEL.addEventListener("click", function () {
+      answerChecker(1);
+      questionNum++;
+      quiz();
+    });
+    a3BtnEL.addEventListener("click", function () {
+      answerChecker(2);
+      questionNum++;
+      quiz();
+    });
+    a4BtnEL.addEventListener("click", function () {
+      answerChecker(3);
+      questionNum++;
+      quiz();
+    });
+  } //if
+  else if (questionNum == 5) {
+    timeRemaining = totalTime;
+    totalTime = 0;
+    final();
+  } else {
+    console.log("Done");
+    localStorage.setItem("Score", score);
+  } //else
+}
+//DISPLAY FUNCTION
+function start() {
+  containerDIVEL.removeChild(quizBL); //removing the quiz
+  containerDIVEL.removeChild(finalBL); //removing the final message
+  //awaiting for button click to start both timer and the quiz
+  startBTNEL.addEventListener("click", function () {
+    containerDIVEL.removeChild(startBL);
+    containerDIVEL.appendChild(quizBL);
+    timeCounter();
+    quiz();
+  });
+}
+start(); //already calling on this from behind
---
+++ scores.js
@@ -0,0 +1,30 @@
+//Grab Data Need to append
+var scoreList = document.getElementById('scoreList');
+var goBackBtnEL = document.getElementById('goBack');
+var clearHBtnEL = document.getElementById('clearHistory');
+
+function displayScore (){
+    var data = JSON.parse(localStorage.getItem("allScores"));
+    console.log(data);
+    if(data != null){
+    for (var i = 0; i < data.length; i++) {
+        var name = data[i].initials;
+        var score = data[i].score; 
+
+        var liEL= document.createElement('li');
+        liEL.textContent = (i+1) +". " + name + "   " + score;
+        scoreList.append(liEL);     
+    }
+    }
+}
+displayScore();
+
+// resets back to the previous page 
+goBackBtnEL.addEventListener("click" , function(){
+    location.href = "index.html"; 
+})
+
+clearHBtnEL.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload(); //this lets you reload the page in it's current html page 
+})
